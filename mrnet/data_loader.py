2f8e51b945c79b911b821711b5b65a555112d89a
MisaOgura <misa.ogura01@gmail.com>
2019-06-19 21:26:25 +0200

    Move empty tensor to device

--- mrnet/data_loader.py
+++ mrnet/data_loader.py
@@ -3,10 +3,13 @@ from torch.utils.data import DataLoader
 from dataset import make_datasets
 
 
-def make_data_loaders(data_dir, plane, diagnosis, batch_size):
+def make_data_loaders(data_dir, plane, diagnosis, batch_size, device=None):
+    if device is None:
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
     train_dataset, valid_dataset = make_datasets(data_dir, plane, diagnosis)
 
-    train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
-    valid_loader = DataLoader(valid_dataset, batch_size)
+    train_loader = DataLoader(train_dataset, batch_size, shuffle=True, device)
+    valid_loader = DataLoader(valid_dataset, batch_size, device)
 
     return train_loader, valid_loader
