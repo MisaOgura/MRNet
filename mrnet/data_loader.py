e8051849118351e180dfdf2c760c76fa532e2d93
MisaOgura <misa.ogura01@gmail.com>
2019-06-19 21:28:22 +0200

    Put positional arguments first

--- mrnet/data_loader.py
+++ mrnet/data_loader.py
@@ -9,7 +9,7 @@ def make_data_loaders(data_dir, plane, diagnosis, batch_size, device=None):
 
     train_dataset, valid_dataset = make_datasets(data_dir, plane, diagnosis)
 
-    train_loader = DataLoader(train_dataset, batch_size, shuffle=True, device)
-    valid_loader = DataLoader(valid_dataset, batch_size, device)
+    train_loader = DataLoader(train_dataset, batch_size, shuffle=True, device=device)
+    valid_loader = DataLoader(valid_dataset, batch_size, device=device)
 
     return train_loader, valid_loader
