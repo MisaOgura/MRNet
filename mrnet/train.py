1f2d8c2f1de529fc29274ef570ee5b8de0040636
MisaOgura <misa.ogura01@gmail.com>
2019-06-20 12:39:26 +0200

    Step back and make it work for one model

--- mrnet/train.py
+++ mrnet/train.py
@@ -54,8 +54,6 @@ def main(data_dir, plane, epochs, batch_size, lr, weight_decay, device=None):
     model_acl = MRNet().to(device)
     model_meniscus = MRNet().to(device)
 
-    models = [model_abnormal, model_acl, model_meniscus]
-
     criterion = nn.BCELoss()
 
     optimizers = [
@@ -77,38 +75,38 @@ def main(data_dir, plane, epochs, batch_size, lr, weight_decay, device=None):
         for inputs, labels in train_loader:
             inputs, labels = inputs.to(device), labels.to(device)
 
-            train_abnormal_loss =forward_and_backprop(model_abnormal,
-                                                      inputs,
-                                                      labels[:,0],
-                                                      criterion,
-                                                      optimizers[0])
+            loss = forward_and_backprop(model_abnormal,
+                                        inputs,
+                                        labels[:,0],
+                                        criterion,
+                                        optimizers[0])
 
-            train_acl_loss =forward_and_backprop(model_acl,
-                                                 inputs,
-                                                 labels[:,1],
-                                                 criterion,
-                                                 optimizers[1])
+            # train_acl_loss = forward_and_backprop(model_acl,
+            #                                      inputs,
+            #                                      labels[:,1],
+            #                                      criterion,
+            #                                      optimizers[1])
 
-            train_meniscus_loss =forward_and_backprop(model_meniscus,
-                                                      inputs,
-                                                      labels[:,2],
-                                                      criterion,
-                                                      optimizers[2])
+            # train_meniscus_loss = forward_and_backprop(model_meniscus,
+            #                                           inputs,
+            #                                           labels[:,2],
+            #                                           criterion,
+            #                                           optimizers[2])
 
             # TODO - scale losses inversely proportionally to the
             # prevelence of the corresponding conditions
 
-            loss = train_abnormal_loss + train_acl_loss + train_meniscus_loss
+            # loss = train_abnormal_loss + train_acl_loss + train_meniscus_loss
             train_loss += loss
 
         for inputs, labels in valid_loader:
             inputs, labels = inputs.to(device), labels.to(device)
 
-            valid_abnormal_loss = forward(model_abnormal, inputs, labels[:,0], criterion)
-            valid_acl_loss = forward(model_acl, inputs, labels[:,1], criterion)
-            valid_meniscus_loss = forward(model_meniscus, inputs, labels[:,2], criterion)
+            loss = forward(model_abnormal, inputs, labels[:,0], criterion)
+            # valid_acl_loss = forward(model_acl, inputs, labels[:,1], criterion)
+            # valid_meniscus_loss = forward(model_meniscus, inputs, labels[:,2], criterion)
 
-            loss = valid_abnormal_loss + valid_acl_loss + valid_meniscus_loss
+            # loss = valid_abnormal_loss + valid_acl_loss + valid_meniscus_loss
             valid_loss += loss
 
         train_loss = train_loss/len(train_loader)
@@ -117,21 +115,21 @@ def main(data_dir, plane, epochs, batch_size, lr, weight_decay, device=None):
         valid_loss = valid_loss/len(valid_loader)
         valid_losses.append(valid_loss)
 
-        print(f'Epoch {epoch + 1}/{epochs}: train loss - {train_loss:.3f}, valid loss - {valid_loss:.3f}')
+        print(f'Epoch {epoch + 1}/{epochs} - train loss: {train_loss:.3f}, valid loss: {valid_loss:.3f}')
 
         if valid_loss < min_valid_loss:
             print(f'\tValidation loss decreased {min_valid_loss:.3f} --> {valid_loss:.3f}.')
 
-            checkpoint = {
-                'epoch': epoch + 1,
-                'state_dict': model.state_dict(),
-                'optimizer': optimizer.state_dict()
-            }
+            # checkpoint = {
+            #     'epoch': epoch + 1,
+            #     'state_dict': model_abnormal.state_dict(),
+            #     'optimizer': optimizer.state_dict()
+            # }
 
-            if not os.path.exists('../models'):
-                os.mkdir('../models')
+            # if not os.path.exists('./models'):
+            #     os.mkdir('./models')
 
-            torch.save(checkpoint, f'../models/model_flower_{now}.pt')
+            # torch.save(checkpoint, f'./models/mrnet_{now}.pt')
 
             min_valid_loss = valid_loss
 
