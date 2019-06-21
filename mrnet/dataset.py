1846c2b734e1826102d7c5d847f38dd08efc5dff
MisaOgura <misa.ogura01@gmail.com>
2019-06-21 11:01:36 +0200

    Remove commented out code

--- mrnet/dataset.py
+++ mrnet/dataset.py
@@ -10,15 +10,12 @@ from torchvision import transforms
 
 
 class MRNetDataset(Dataset):
-    planes = ['axial', 'coronal', 'sagittal']
-
     def __init__(self, dataset_dir, labels_path, plane, transform=None, device=None):
         self.case_paths = sorted(glob(f'{dataset_dir}/**'))
         self.labels_df = pd.read_csv(labels_path)
         self.plane = plane
         self.transform = transform
         self.window = 7
-
         self.device = device
         if self.device is None:
             self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -28,23 +25,13 @@ class MRNetDataset(Dataset):
 
     def __getitem__(self, idx):
         case_path = self.case_paths[idx]
-        series = sorted(glob(f'{case_path}/{self.plane}/*.png'))
-
-        # Get indecies of middle 15 slices
-
-        # mid_idx = len(series) // 2
-        # from_idx = mid_idx - self.window
-        # to_idx = mid_idx + self.window + 1
-        # image_paths = series[from_idx:to_idx]
-        image_paths = series
-
+        image_paths = sorted(glob(f'{case_path}/{self.plane}/*.png'))
         data = torch.tensor([]).to(self.device)
 
         for path in image_paths:
             image = Image.open(path)
             if self.transform is not None:
                 image = self.transform(image).unsqueeze(0).to(self.device)
-
             data = torch.cat((data, image), 0)
 
         diagnoses = self.labels_df.iloc[idx].values[1:].astype(np.float32)
