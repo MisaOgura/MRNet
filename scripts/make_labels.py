885467272382b088a422f4849e1575c49b559cd6
MisaOgura <misa.ogura01@gmail.com>
2019-06-21 16:01:20 +0200

    Label colums correctly with conditions

--- scripts/make_labels.py
+++ scripts/make_labels.py
@@ -5,24 +5,24 @@ import pandas as pd
 import numpy as np
 
 
-def load_csv(csv_path):
+def load_csv(data_type, condition):
+    csv_path = f'{data_dir}/{data_type}-{condition}.csv'
     return pd.read_csv(csv_path,
                        header=None,
-                       names=['case', 'abnormal'],
-                       dtype={'case': str, 'abnormal': np.int64})
-
+                       names=['case', condition],
+                       dtype={'case': str, f'{condition}': np.int64})
 
 def main(data_dir, out_dir):
-    train_abnormal_df = load_csv(f'{data_dir}/train-abnormal.csv')
-    train_acl_df = load_csv(f'{data_dir}/train-acl.csv')
-    train_meniscus_df = load_csv(f'{data_dir}/train-meniscus.csv')
+    train_abnormal_df = load_csv('train', 'abnormal')
+    train_acl_df = load_csv('train', 'acl')
+    train_meniscus_df = load_csv('train', 'meniscus')
 
     train_df = pd.merge(train_abnormal_df, train_acl_df, on='case') \
                  .merge(train_meniscus_df, on='case')
 
-    valid_abnormal_df = load_csv(f'{data_dir}/valid-abnormal.csv')
-    valid_acl_df = load_csv(f'{data_dir}/valid-acl.csv')
-    valid_meniscus_df = load_csv(f'{data_dir}/valid-meniscus.csv')
+    valid_abnormal_df = load_csv('valid', 'abnormal')
+    valid_acl_df = load_csv('valid', 'acl')
+    valid_meniscus_df = load_csv('valid', 'meniscus')
 
     valid_df = pd.merge(valid_abnormal_df, valid_acl_df, on='case') \
                  .merge(valid_meniscus_df, on='case')
