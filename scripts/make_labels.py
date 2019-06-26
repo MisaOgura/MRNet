b1103cc78ad658b7479d65a06a03c98ef727429d
MisaOgura <misa.ogura01@gmail.com>
2019-06-26 13:29:57 +0100

    Don't pass in output dir

--- scripts/make_labels.py
+++ scripts/make_labels.py
@@ -12,7 +12,7 @@ def load_csv(data_type, condition):
                        names=['case', condition],
                        dtype={'case': str, f'{condition}': np.int64})
 
-def main(data_dir, out_dir):
+def main(data_dir):
     train_abnormal_df = load_csv('train', 'abnormal')
     train_acl_df = load_csv('train', 'acl')
     train_meniscus_df = load_csv('train', 'meniscus')
@@ -27,19 +27,18 @@ def main(data_dir, out_dir):
     valid_df = pd.merge(valid_abnormal_df, valid_acl_df, on='case') \
                  .merge(valid_meniscus_df, on='case')
 
-    train_df.to_csv(f'{out_dir}/train_labels.csv', index=False)
-    valid_df.to_csv(f'{out_dir}/valid_labels.csv', index=False)
+    train_df.to_csv(f'{data_dir}/train_labels.csv', index=False)
+    valid_df.to_csv(f'{data_dir}/valid_labels.csv', index=False)
 
-    print(f"Created 'train_labels.csv' and 'valid_labels.csv' in {out_dir}")
+    print(f"Created 'train_labels.csv' and 'valid_labels.csv' in {data_dir}")
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: python3 make_labels.py <data_dir> <out_dir>')
-        print('e.g. python3 make_labels.py data/MRNet-v1.0 data/processed')
+    if len(sys.argv) != 2:
+        print('Usage: python3 make_labels.py <data_dir>')
+        print('e.g. python3 make_labels.py MRNet-v1.0')
         exit(1)
 
     data_dir = sys.argv[1]
-    out_dir = sys.argv[2]
 
-    main(data_dir, out_dir)
+    main(data_dir)
